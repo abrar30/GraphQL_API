69c81660c8858f3b8b0d5f1c1c123de089166889
abrar330 <abrarhussain@ifca.com.my>
2020-05-12 16:03:20 +0800

    first commit

--- node_modules/@apollo/gateway/dist/utilities/array.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function compactMap<T, U>(array: T[], callbackfn: (value: T, index: number, array: T[]) => U | null | undefined): U[];
-export declare function partition<T>(array: T[], predicate: (element: T, index: number, array: T[]) => boolean): [T[], T[]];
-export declare function findAndExtract<T>(array: T[], predicate: (element: T, index: number, array: T[]) => boolean): [T | undefined, T[]];
-//# sourceMappingURL=array.d.ts.map
